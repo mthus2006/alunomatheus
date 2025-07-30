c21d3d2106ba3e3c587199a30fc2178db5352a0f
mthus2006 <166153564+mthus2006@users.noreply.github.com>
2025-07-30 13:35:58 -0300

    hjh

---
+++ script.js
@@ -0,0 +1,6 @@
+function inserirNome(){
+    let nomeUsuario = prompt ("qual o seu nome");
+    let elemento = document.querySelector("#nome-usuario");
+    console.log(elemento);
+    elemento.textContent = nomeUsuario,
+}
