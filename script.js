a7e2a6ffa9eb2922ab346572228f77a9e1f87842
mthus2006 <166153564+mthus2006@users.noreply.github.com>
2025-08-06 13:24:36 -0300

    gg

--- script.js
+++ script.js
@@ -19,6 +19,11 @@ let aluno1 ={
 }
 
 console.log(aluno1.nome);
-console.log(aluno1.idade),
+console.log(aluno1.idade);
 console.log(aluno1.materiasFavoritas);
-console.log()
+console.log(aluno1.materiasFavoritas[1]);
+
+const objeto = document.querySelector('aluno');
+objeto.textContent = aluno1.nome;
+objeto.textContent = aluno1.idade;
+objeto.textContent = aluno1.materiasFavoritas[1];
